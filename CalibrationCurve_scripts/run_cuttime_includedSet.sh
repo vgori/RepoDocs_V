ea0cefe4398b647d4e29914a95bb8b05a6d34ac7
vgori <splaijna@gmail.com>
2024-08-19 17:24:57 +0300

    sets updated

--- CalibrationCurve_scripts/run_cuttime_includedSet.sh
+++ CalibrationCurve_scripts/run_cuttime_includedSet.sh
@@ -4,7 +4,7 @@
 # ./run_cuttime_includedSet.sh /media/localarchive/ONT-data/4th-calibration /media/localarchive/ONT-data/4th-calibration/Cutoffs 5 10 15 30 45 60 90
 # Input folder containing subfolders (replace with your actual folder names)
 # Next folders "H14-3Gy" "P1-4Gy" "H14-2Gy" "P1-2Gy" "P1-3Gy" "P1-5Gy" "P32-01Gy" "P32-025Gy"
-folders=("H14-0Gy" "P1-0Gy" "P32-0Gy" "H14-05Gy" "P1-05Gy" "P32-05Gy" "H14-1Gy" "P1-1Gy" "P32-1Gy-repeat")
+folders=("H14-4Gy" "H14-5Gy" "P1-01Gy" "P32-2Gy" "P32-3Gy" "P32-4Gy")
 your_command="./Cutoff_CORE.sh"
 #your_command="./Cutoff.sh"
 
